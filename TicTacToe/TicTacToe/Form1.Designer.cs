710defa3bd49ca8494af2f373bbb5ea9e1b5de51
LupulNebun <115409348+LupulNebun@users.noreply.github.com>
2022-11-14 18:59:48 +0200

    Add files via upload

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Check_1 = new System.Windows.Forms.Button();
+            this.btn_Check_2 = new System.Windows.Forms.Button();
+            this.btn_Check_3 = new System.Windows.Forms.Button();
+            this.btn_Check_6 = new System.Windows.Forms.Button();
+            this.btn_Check_5 = new System.Windows.Forms.Button();
+            this.btn_Check_4 = new System.Windows.Forms.Button();
+            this.btn_Check_9 = new System.Windows.Forms.Button();
+            this.btn_Check_8 = new System.Windows.Forms.Button();
+            this.btn_Check_7 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_Player2_Score = new System.Windows.Forms.Label();
+            this.lbl_Player1_Score = new System.Windows.Forms.Label();
+            this.lbl_Player1 = new System.Windows.Forms.Label();
+            this.lbl_Player2 = new System.Windows.Forms.Label();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Check_1
+            // 
+            this.btn_Check_1.AutoSize = true;
+            this.btn_Check_1.Location = new System.Drawing.Point(12, 142);
+            this.btn_Check_1.Name = "btn_Check_1";
+            this.btn_Check_1.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_1.TabIndex = 0;
+            this.btn_Check_1.UseVisualStyleBackColor = true;
+            this.btn_Check_1.Click += new System.EventHandler(this.btn_Check_1_Click);
+            // 
+            // btn_Check_2
+            // 
+            this.btn_Check_2.AutoSize = true;
+            this.btn_Check_2.Location = new System.Drawing.Point(188, 142);
+            this.btn_Check_2.Name = "btn_Check_2";
+            this.btn_Check_2.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_2.TabIndex = 1;
+            this.btn_Check_2.UseVisualStyleBackColor = true;
+            this.btn_Check_2.Click += new System.EventHandler(this.btn_Check_2_Click_1);
+            // 
+            // btn_Check_3
+            // 
+            this.btn_Check_3.AutoSize = true;
+            this.btn_Check_3.Location = new System.Drawing.Point(348, 142);
+            this.btn_Check_3.Name = "btn_Check_3";
+            this.btn_Check_3.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_3.TabIndex = 2;
+            this.btn_Check_3.UseVisualStyleBackColor = true;
+            this.btn_Check_3.Click += new System.EventHandler(this.btn_Check_3_Click);
+            // 
+            // btn_Check_6
+            // 
+            this.btn_Check_6.AutoSize = true;
+            this.btn_Check_6.Location = new System.Drawing.Point(348, 316);
+            this.btn_Check_6.Name = "btn_Check_6";
+            this.btn_Check_6.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_6.TabIndex = 5;
+            this.btn_Check_6.UseVisualStyleBackColor = true;
+            this.btn_Check_6.Click += new System.EventHandler(this.btn_Check_6_Click);
+            // 
+            // btn_Check_5
+            // 
+            this.btn_Check_5.AutoSize = true;
+            this.btn_Check_5.Location = new System.Drawing.Point(188, 316);
+            this.btn_Check_5.Name = "btn_Check_5";
+            this.btn_Check_5.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_5.TabIndex = 4;
+            this.btn_Check_5.UseVisualStyleBackColor = true;
+            this.btn_Check_5.Click += new System.EventHandler(this.btn_Check_5_Click);
+            // 
+            // btn_Check_4
+            // 
+            this.btn_Check_4.AutoSize = true;
+            this.btn_Check_4.Location = new System.Drawing.Point(12, 316);
+            this.btn_Check_4.Name = "btn_Check_4";
+            this.btn_Check_4.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_4.TabIndex = 3;
+            this.btn_Check_4.UseVisualStyleBackColor = true;
+            this.btn_Check_4.Click += new System.EventHandler(this.btn_Check_4_Click);
+            // 
+            // btn_Check_9
+            // 
+            this.btn_Check_9.AutoSize = true;
+            this.btn_Check_9.Location = new System.Drawing.Point(348, 474);
+            this.btn_Check_9.Name = "btn_Check_9";
+            this.btn_Check_9.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_9.TabIndex = 8;
+            this.btn_Check_9.UseVisualStyleBackColor = true;
+            this.btn_Check_9.Click += new System.EventHandler(this.btn_Check_9_Click);
+            // 
+            // btn_Check_8
+            // 
+            this.btn_Check_8.AutoSize = true;
+            this.btn_Check_8.Location = new System.Drawing.Point(188, 474);
+            this.btn_Check_8.Name = "btn_Check_8";
+            this.btn_Check_8.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_8.TabIndex = 7;
+            this.btn_Check_8.UseVisualStyleBackColor = true;
+            this.btn_Check_8.Click += new System.EventHandler(this.btn_Check_8_Click);
+            // 
+            // btn_Check_7
+            // 
+            this.btn_Check_7.AutoSize = true;
+            this.btn_Check_7.Location = new System.Drawing.Point(12, 474);
+            this.btn_Check_7.Name = "btn_Check_7";
+            this.btn_Check_7.Size = new System.Drawing.Size(100, 100);
+            this.btn_Check_7.TabIndex = 6;
+            this.btn_Check_7.UseVisualStyleBackColor = true;
+            this.btn_Check_7.Click += new System.EventHandler(this.btn_Check_7_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.lbl_Player2_Score);
+            this.panel1.Controls.Add(this.lbl_Player1_Score);
+            this.panel1.Controls.Add(this.lbl_Player1);
+            this.panel1.Controls.Add(this.lbl_Player2);
+            this.panel1.Controls.Add(this.btn_Reset);
+            this.panel1.Location = new System.Drawing.Point(-3, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(465, 131);
+            this.panel1.TabIndex = 9;
+            // 
+            // lbl_Player2_Score
+            // 
+            this.lbl_Player2_Score.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl_Player2_Score.Location = new System.Drawing.Point(339, 65);
+            this.lbl_Player2_Score.Name = "lbl_Player2_Score";
+            this.lbl_Player2_Score.Size = new System.Drawing.Size(123, 28);
+            this.lbl_Player2_Score.TabIndex = 5;
+            this.lbl_Player2_Score.Text = "0";
+            this.lbl_Player2_Score.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Player1_Score
+            // 
+            this.lbl_Player1_Score.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl_Player1_Score.Location = new System.Drawing.Point(3, 67);
+            this.lbl_Player1_Score.Name = "lbl_Player1_Score";
+            this.lbl_Player1_Score.Size = new System.Drawing.Size(123, 28);
+            this.lbl_Player1_Score.TabIndex = 4;
+            this.lbl_Player1_Score.Text = "0";
+            this.lbl_Player1_Score.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Player1_Score.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbl_Player1
+            // 
+            this.lbl_Player1.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl_Player1.Location = new System.Drawing.Point(3, 39);
+            this.lbl_Player1.Name = "lbl_Player1";
+            this.lbl_Player1.Size = new System.Drawing.Size(123, 28);
+            this.lbl_Player1.TabIndex = 3;
+            this.lbl_Player1.Text = "Player 1:";
+            this.lbl_Player1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Player2
+            // 
+            this.lbl_Player2.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbl_Player2.Location = new System.Drawing.Point(339, 39);
+            this.lbl_Player2.Name = "lbl_Player2";
+            this.lbl_Player2.Size = new System.Drawing.Size(126, 28);
+            this.lbl_Player2.TabIndex = 2;
+            this.lbl_Player2.Text = "Player 2:";
+            this.lbl_Player2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Location = new System.Drawing.Point(175, 39);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(130, 54);
+            this.btn_Reset.TabIndex = 0;
+            this.btn_Reset.Text = "RESET";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(23)))), ((int)(((byte)(91)))));
+            this.ClientSize = new System.Drawing.Size(461, 597);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_Check_9);
+            this.Controls.Add(this.btn_Check_8);
+            this.Controls.Add(this.btn_Check_7);
+            this.Controls.Add(this.btn_Check_6);
+            this.Controls.Add(this.btn_Check_5);
+            this.Controls.Add(this.btn_Check_4);
+            this.Controls.Add(this.btn_Check_3);
+            this.Controls.Add(this.btn_Check_2);
+            this.Controls.Add(this.btn_Check_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Check_1;
+        private System.Windows.Forms.Button btn_Check_2;
+        private System.Windows.Forms.Button btn_Check_3;
+        private System.Windows.Forms.Button btn_Check_6;
+        private System.Windows.Forms.Button btn_Check_5;
+        private System.Windows.Forms.Button btn_Check_4;
+        private System.Windows.Forms.Button btn_Check_9;
+        private System.Windows.Forms.Button btn_Check_8;
+        private System.Windows.Forms.Button btn_Check_7;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_Player2;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Label lbl_Player1;
+        private System.Windows.Forms.Label lbl_Player1_Score;
+        private System.Windows.Forms.Label lbl_Player2_Score;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool player = true;
+        int count = 0, P1_point = 1, P2_point = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Reset()
+        {
+            player = true;
+            count = 0;
+            btn_Check_1.Text = " ";
+            btn_Check_2.Text = " ";
+            btn_Check_3.Text = " ";
+            btn_Check_4.Text = " ";
+            btn_Check_5.Text = " ";
+            btn_Check_6.Text = " ";
+            btn_Check_7.Text = " ";
+            btn_Check_8.Text = " ";
+            btn_Check_9.Text = " ";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void B_text(Button B_Text)
+        {
+            if(B_Text.Text != "X" && B_Text.Text != "O")
+            {
+                if (player)
+                {
+                    B_Text.Text = "X";
+                    player = false;
+                    count++;
+                    if (count >= 3)
+                    {
+                        if (btn_Check_1.Text == "X" && btn_Check_2.Text == "X" && btn_Check_3.Text == "X")
+                        {
+                            lbl_Player1_Score.Text = P1_point.ToString();
+                            P1_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_4.Text == "X" && btn_Check_5.Text == "X" && btn_Check_6.Text == "X")
+                        {
+                            lbl_Player1_Score.Text = P1_point.ToString();
+                            P1_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_7.Text == "X" && btn_Check_8.Text == "X" && btn_Check_9.Text == "X")
+                        {
+                            lbl_Player1_Score.Text = P1_point.ToString();
+                            P1_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_1.Text == "X" && btn_Check_4.Text == "X" && btn_Check_7.Text == "X")
+                        {
+                            lbl_Player1_Score.Text = P1_point.ToString();
+                            P1_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_2.Text == "X" && btn_Check_5.Text == "X" && btn_Check_8.Text == "X")
+                        {
+                            lbl_Player1_Score.Text = P1_point.ToString();
+                            P1_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_3.Text == "X" && btn_Check_6.Text == "X" && btn_Check_9.Text == "X")
+                        {
+                            lbl_Player1_Score.Text = P1_point.ToString();
+                            P1_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_1.Text == "X" && btn_Check_5.Text == "X" && btn_Check_9.Text == "X")
+                        {
+                            lbl_Player1_Score.Text = P1_point.ToString();
+                            P1_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_3.Text == "X" && btn_Check_5.Text == "X" && btn_Check_7.Text == "X")
+                        {
+                            lbl_Player1_Score.Text = P1_point.ToString();
+                            P1_point++;
+                            Reset();
+                        }
+                    }
+                }
+                else
+                {
+                    B_Text.Text = "O";
+                    player = true;
+                    if (count >= 3)
+                    {
+                        if (btn_Check_1.Text == "O" && btn_Check_2.Text == "O" && btn_Check_3.Text == "O")
+                        {
+                            lbl_Player2_Score.Text = P2_point.ToString();
+                            P2_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_4.Text == "O" && btn_Check_5.Text == "O" && btn_Check_6.Text == "O")
+                        {
+                            lbl_Player2_Score.Text = P2_point.ToString();
+                            P2_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_7.Text == "O" && btn_Check_8.Text == "O" && btn_Check_9.Text == "O")
+                        {
+                            lbl_Player2_Score.Text = P2_point.ToString();
+                            P2_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_1.Text == "O" && btn_Check_4.Text == "O" && btn_Check_7.Text == "O")
+                        {
+                            lbl_Player2_Score.Text = P2_point.ToString();
+                            P2_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_2.Text == "O" && btn_Check_5.Text == "O" && btn_Check_8.Text == "O")
+                        {
+                            lbl_Player2_Score.Text = P2_point.ToString();
+                            P2_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_3.Text == "O" && btn_Check_6.Text == "O" && btn_Check_9.Text == "O")
+                        {
+                            lbl_Player2_Score.Text = P2_point.ToString();
+                            P2_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_1.Text == "O" && btn_Check_5.Text == "O" && btn_Check_9.Text == "O")
+                        {
+                            lbl_Player2_Score.Text = P2_point.ToString();
+                            P2_point++;
+                            Reset();
+                        }
+                        else if (btn_Check_3.Text == "O" && btn_Check_5.Text == "O" && btn_Check_7.Text == "O")
+                        {
+                            lbl_Player2_Score.Text = P2_point.ToString();
+                            P2_point++;
+                            Reset();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btn_Check_1_Click(object sender, EventArgs e)
+        {
+            B_text(btn_Check_1);
+        }
+
+        private void btn_Check_2_Click_1(object sender, EventArgs e)
+        {
+            B_text(btn_Check_2);
+        }
+
+        private void btn_Check_3_Click(object sender, EventArgs e)
+        {
+            B_text(btn_Check_3);
+        }
+
+        private void btn_Check_4_Click(object sender, EventArgs e)
+        {
+            B_text(btn_Check_4);
+        }
+
+        private void btn_Check_5_Click(object sender, EventArgs e)
+        {
+            B_text(btn_Check_5);
+        }
+
+        private void btn_Check_6_Click(object sender, EventArgs e)
+        {
+            B_text(btn_Check_6);
+        }
+
+        private void btn_Check_7_Click(object sender, EventArgs e)
+        {
+            B_text(btn_Check_7);
+        }
+
+        private void btn_Check_8_Click(object sender, EventArgs e)
+        {
+            B_text(btn_Check_8);
+        }
+
+        private void btn_Check_9_Click(object sender, EventArgs e)
+        {
+            B_text(btn_Check_9);
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            player = true;
+            count = 0;
+            btn_Check_1.Text = " ";
+            btn_Check_2.Text = " ";
+            btn_Check_3.Text = " ";
+            btn_Check_4.Text = " ";
+            btn_Check_5.Text = " ";
+            btn_Check_6.Text = " ";
+            btn_Check_7.Text = " ";
+            btn_Check_8.Text = " ";
+            btn_Check_9.Text = " ";
+        }
+
+    }
+}
